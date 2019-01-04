d9576a38b895c2cb4f9c9da6c582c208c218e1a9
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-03 19:41:49 -0600

    Intro to react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function Home() {
+  return <div>Home</div>
+}
 
 function App() {
-  return <div>APP COMPONENT</div>;
+  return (
+    <div>
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
+    </div>
+  )
 }
 export default App;
