efc7a103e3707ffed50577e48109580d1cb83ca4
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-04 18:53:02 -0600

    routes of my app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function Home() {
-  return <div>Home</div>
-}
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 function App() {
   return (
     <div>
+      <div>Header</div>
       <Router>
-        <Route exact path="/" component={Home} />
+        <Switch>
+          <Route exact path="/" component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </Switch>
       </Router>
     </div>
   )
