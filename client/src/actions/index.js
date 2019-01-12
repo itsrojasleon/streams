33480c558d83012ef6629025f91d3812bcebff66
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-12 17:59:35 -0600

    Dispatching actions after stream creation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,11 @@
 import streams from "../apis/streams";
 
-import { SIGN_IN, SIGN_OUT } from "./types";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
 
 export const signIn = userId => ({ type: SIGN_IN, payload: userId });
 export const signOut = () => ({ type: SIGN_OUT });
 
 export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues);
+  const { data } = await streams.post('/streams', formValues);
+  dispatch({ type: CREATE_STREAM, payload: data });
 };
