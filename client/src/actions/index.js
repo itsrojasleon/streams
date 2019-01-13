47eac91483c7d05060e770ca17c54ebeca09380a
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-13 14:22:53 -0600

    Bulk Action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,13 @@
-import streams from "../apis/streams";
+import streams from '../apis/streams';
 
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+  SIGN_IN, SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+  } from './types';
 
 export const signIn = userId => ({ type: SIGN_IN, payload: userId });
 export const signOut = () => ({ type: SIGN_OUT });
@@ -9,3 +16,23 @@ export const createStream = formValues => async dispatch => {
   const { data } = await streams.post('/streams', formValues);
   dispatch({ type: CREATE_STREAM, payload: data });
 };
+
+export const fetchStreams = () => async dispatch => {
+  const { data } = await streams.get('/streams');
+  dispatch({ type: FETCH_STREAMS, payload: data });
+}
+
+export const fetchStream = id => async dispatch => {
+  const { data } = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: data });
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+  const { data } = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: data });
+}
+
+export const deleteStream = (id) => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id })
+}
