bed37b527ae56b84684548e76579452ac86ff430
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-16 17:56:54 -0600

    Conditionally showing edit and delete

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -7,10 +7,21 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+  renderAdmin = (stream) => {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
   renderList = () => {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">{stream.title}
             <div className="description">{stream.description}</div>
@@ -30,7 +41,10 @@ class StreamList extends React.Component {
     );
   }
 }
-const mapStateToProps = ({ streams }) => {
-  return { streams: Object.values(streams) }
+const mapStateToProps = ({ streams, auth }) => {
+  return {
+    streams: Object.values(streams),
+    currentUserId: auth.userId
+  }
 }
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
