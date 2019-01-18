70008576bc3d0c4c9b685747e39c5ea675f47c9b
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-18 17:50:48 -0600

    Avoiding changes to properties

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions/index';
+import _ from 'lodash';
+
+import { fetchStream, editStream } from '../../actions/index';
+
+import StreamForm from '../streams/StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+  }
+  onSubmit = formValues => {
+    this.props.editStream(formValues)
   }
   render() {
     if (!this.props.stream)
       return <div>Loading...</div> 
     return (
       <div>
-        <div>{this.props.stream.description}</div>
+        <h3>Edit a stream</h3>
+        <StreamForm 
+          onSubmit={this.onSubmit} 
+          initialValues={}
+        />
       </div>
     );
   }
@@ -19,4 +30,4 @@ class StreamEdit extends React.Component {
 const mapStateToProps = ({ streams }, ownProps) => ({
   stream: streams[ownProps.match.params.id]
 });
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
