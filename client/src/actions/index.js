d9d58c45685d842892ddae9c5b4c22b93774e16b
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-18 18:02:53 -0600

    PUT vs PATCH request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -32,7 +32,7 @@ export const fetchStream = id => async dispatch => {
 }
 
 export const editStream = (id, formValues) => async dispatch => {
-  const { data } = await streams.put(`/streams/${id}`, formValues);
+  const { data } = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: data });
   history.push('/');
 }
