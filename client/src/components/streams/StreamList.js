94fe2a7a5ef7f72a47864f95c8b001718a580b0a
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-22 20:11:42 -0600

    Switches with react-router

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -20,13 +20,16 @@ class StreamList extends React.Component {
       );
     }
   }
+
+  // List of streams
   renderList = () => {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
-          <div className="content">{stream.title}
+          <div className="content">
+            <Link to={`/streams/${stream.id}`} className="header">{stream.title}</Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
