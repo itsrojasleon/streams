d3b2b44510aa038837f18c83a5b8e5860ca83fd2
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-22 20:23:03 -0600

    Showing a stream

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -22,7 +22,7 @@ function App() {
             <Route path="/streams/new" component={StreamCreate} />
             <Route path="/streams/edit/:id" component={StreamEdit} />
             <Route path="/streams/delete/:id" component={StreamDelete} />
-            <Route path="/streams/show/:id" component={StreamShow} />
+            <Route path="/streams/:id" component={StreamShow} />
           </Switch>
         </>
       </Router>
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions/index';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    const { title, description } = this.props.stream;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
 }
-export default StreamShow;
+const mapStateToProps = ({ streams }, ownProps) => {
+  return { stream: streams[ownProps.match.params.id] }
+}
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
