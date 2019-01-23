aa962948400822ffa10dbe16f57710b40cfcdc50
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-23 17:39:10 -0600

    RTMP server

---
+++ rtmpserver/index.js
@@ -0,0 +1,19 @@
+const { NodeMediaServer } = require('node-media-server');
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 60,
+    ping_timeout: 30
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+};
+const nms = new NodeMediaServer(config);
+
+// Run server
+nms.run();
