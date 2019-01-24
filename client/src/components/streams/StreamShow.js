30833c46db0e37e45ce41762d5b6eca8018e11a2
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-23 18:37:24 -0600

    Optional player building

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -6,19 +6,31 @@ import { fetchStream } from '../../actions/index';
 class StreamShow extends React.Component {
   constructor(props) {
     super(props);
-
     this.videoRef = React.createRef();
   }
+  
   componentDidMount() {
-    const { id } = this.props.match.params;
+    const { id } = this.props.match.params; 
     this.props.fetchStream(id);
+    this.buildPlayer();
+  }
 
-    flv.createPlayer({
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer = () => {
+    if (this.player || !this.props.stream) return;
+
+    const { id } = this.props.match.params; 
+    this.player = flv.createPlayer({
       type: 'flv',
       url: `http://localhost:8000/live/${id}.flv`
     });
     this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>
