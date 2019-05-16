669425290dc2807109140b93bd2a6a0656724388
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-16 01:30:38 -0500

    Let's talk about performance

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,9 +6,13 @@ import GoogleAuth from './GoogleAuth';
 function Header() {
   return (
     <div className="ui secondary pointing menu">
-      <Link to="/" className="item">Streamy</Link>
+      <Link to="/" className="header item">
+        Streamy
+      </Link>
       <div className="right menu">
-        <Link to="/" className="item">All Streams</Link>
+        <Link to="/" className="item">
+          All Streams
+        </Link>
         <GoogleAuth />
       </div>
     </div>
